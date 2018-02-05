6c2b59967c970739801a1dae6eefb3e38319084d
Sana Shaikh <sa28na@gmail.com>
2018-02-05 15:29:43 +0530

    Object Oriented

---
+++ src/com/bridgelabz/objectOriented/InventoryDataManagement.java
@@ -0,0 +1,40 @@
+package com.bridgelabz.objectOriented;
+
+import java.io.FileReader;
+/*import java.util.Iterator;
+import org.json.simple.JSONArray;*/
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+public class InventoryDataManagement {
+
+	public static void main(String[] args) {
+		JSONParser parser = new JSONParser();
+		try {
+			Object obj = parser.parse(
+					new FileReader("/home/bridgelabz/SANAworkspace/BasicProgram/src/com/bridgelabz/objectOriented/inventory.json"));
+
+			JSONObject jsonObject = (JSONObject) obj;
+
+				String name = (String) jsonObject.get("Name");
+				System.out.println("Name: " + name);
+
+				String weight = (String) jsonObject.get("Weight");
+				double weight1 = Double.parseDouble(weight);
+				System.out.println("Weight: " + weight1 + " " + "kg");
+
+				String price = (String) jsonObject.get("Price");
+				double price1 = Double.parseDouble(price);
+				System.out.println("Price:" + price1 + " " + "Rs/kg");
+
+				double totalAmount=0;
+				double amount = weight1 * price1;
+				totalAmount = amount + totalAmount;
+				System.out.println("Total Inventary cost.." + totalAmount);
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
